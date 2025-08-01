a03e87c8c980263a6b8c926b540fb03c5cc43128
andyblaze <andy@mediagraphic.co.uk>
2025-08-01 16:51:49 +0100

    more tweaks to the Visualiser class in index.html to use the new Canvas class used for actual drawing. Also re-jigged Dave.getActivations

--- dave.js
+++ dave.js
@@ -83,78 +83,10 @@ class Dave {
         return this.feedForward(input);
     }
     getActivations(number) {
-        const inputs = encodeNumber(number);
-        const output = this.predict(number); // this sets dave.hiddenActivations
-        return {
-            inputs,
-            hidden: this.hiddenActivations,
-            output
-        };
+        return [
+            {"title": "Input", "values": encodeNumber(number)},
+            {"title": "Output", "values": [this.predict(number)]}, // this sets dave.hiddenActivations
+            {"title": "Hidden", "values": this.hiddenActivations}
+        ];
     }
-}
-
-function create(inputSize, hiddenSize, learningRate = 0.1) {
-  return {
-    inputSize,
-    hiddenSize,
-    learningRate,
-    // Initialize weights from input to hidden: hiddenSize x inputSize matrix
-    inputToHiddenWeights: Array.from({ length: hiddenSize }, () =>
-      Array.from({ length: inputSize }, () => Math.random() * 2 - 1)
-    ),
-    hiddenBiases: Array.from({ length: hiddenSize }, () => Math.random() * 2 - 1),
-    // Initialize weights from hidden to output: 1 x hiddenSize vector
-    hiddenToOutputWeights: Array.from({ length: hiddenSize }, () => Math.random() * 2 - 1),
-    outputBias: Math.random() * 2 - 1,
-    hiddenActivations: []
-  };
-}
-
-function feedForward(net, input) {
-  net.hiddenActivations = net.inputToHiddenWeights.map((weights, i) => {
-    let sum = net.hiddenBiases[i];
-    for (let j = 0; j < net.inputSize; j++) {
-      sum += weights[j] * input[j];
-    }
-    return sigmoid(sum);
-  });
-
-  let sumOut = net.outputBias;
-  for (let i = 0; i < net.hiddenSize; i++) {
-    sumOut += net.hiddenActivations[i] * net.hiddenToOutputWeights[i];
-  }
-  return sigmoid(sumOut);
-}
-
-function train(net, input, target) {
-  const output = feedForward(net, input);
-  const error = target - output;
-  const outputGradient = error * sigmoidDerivative(output);
-
-  // Update hidden-output weights and output bias
-  for (let i = 0; i < net.hiddenSize; i++) {
-    net.hiddenToOutputWeights[i] += net.hiddenActivations[i] * outputGradient * net.learningRate;
-  }
-  net.outputBias += outputGradient * net.learningRate;
-
-  // Backpropagate error to hidden layer
-  const hiddenGradients = net.hiddenActivations.map((h, i) => {
-    const errorHidden = outputGradient * net.hiddenToOutputWeights[i];
-    return errorHidden * sigmoidDerivative(h);
-  });
-
-  // Update input-hidden weights and hidden biases
-  for (let i = 0; i < net.hiddenSize; i++) {
-    for (let j = 0; j < net.inputSize; j++) {
-      net.inputToHiddenWeights[i][j] += input[j] * hiddenGradients[i] * net.learningRate;
-    }
-    net.hiddenBiases[i] += hiddenGradients[i] * net.learningRate;
-  }
-
-  return error;
-}
-
-function predict(net, number) {
-    const input = encodeNumber(number);
-  return feedForward(net, input);
 }
