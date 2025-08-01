053ccf5d3557e7e40ae060ebca8bf142125f1f25
andyblaze <andy@mediagraphic.co.uk>
2025-08-01 09:13:10 +0100

    AI is now a class in dave-ai.js, index.html updated to suit

--- dave.js
+++ dave.js
@@ -22,6 +22,68 @@ function encodeNumber(n) {
   ];
 }
 
+class Dave {
+    constructor(inputSize, hiddenSize, learningRate = 0.1) {
+        this.inputSize = inputSize;
+        this.hiddenSize = hiddenSize;
+        this.learningRate = learningRate;
+        // Initialize weights from input to hidden: hiddenSize x inputSize matrix
+        this.inputToHiddenWeights = Array.from({ length: hiddenSize }, () =>
+          Array.from({ length: inputSize }, () => Math.random() * 2 - 1)
+        );
+        this.hiddenBiases = Array.from({ length: hiddenSize }, () => Math.random() * 2 - 1);
+        // Initialize weights from hidden to output: 1 x hiddenSize vector
+        this.hiddenToOutputWeights = Array.from({ length: hiddenSize }, () => Math.random() * 2 - 1);
+        this.outputBias = Math.random() * 2 - 1;
+        this.hiddenActivations = [];
+    }
+    feedForward(input) {
+        this.hiddenActivations = this.inputToHiddenWeights.map((weights, i) => {
+        let sum = this.hiddenBiases[i];
+        for (let j = 0; j < this.inputSize; j++) {
+            sum += weights[j] * input[j];
+        }
+        return sigmoid(sum);
+        });
+
+        let sumOut = this.outputBias;
+        for (let i = 0; i < this.hiddenSize; i++) {
+            sumOut += this.hiddenActivations[i] * this.hiddenToOutputWeights[i];
+        }
+        return sigmoid(sumOut);
+    }
+    train(input, target) {
+        const output = this.feedForward(input);
+        const error = target - output;
+        const outputGradient = error * sigmoidDerivative(output);
+
+        // Update hidden-output weights and output bias
+        for (let i = 0; i < this.hiddenSize; i++) {
+            this.hiddenToOutputWeights[i] += this.hiddenActivations[i] * outputGradient * this.learningRate;
+        }
+        this.outputBias += outputGradient * this.learningRate;
+
+        // Backpropagate error to hidden layer
+        const hiddenGradients = this.hiddenActivations.map((h, i) => {
+            const errorHidden = outputGradient * this.hiddenToOutputWeights[i];
+            return errorHidden * sigmoidDerivative(h);
+        });
+
+        // Update input-hidden weights and hidden biases
+        for (let i = 0; i < this.hiddenSize; i++) {
+            for (let j = 0; j < this.inputSize; j++) {
+                this.inputToHiddenWeights[i][j] += input[j] * hiddenGradients[i] * this.learningRate;
+            }
+            this.hiddenBiases[i] += hiddenGradients[i] * this.learningRate;
+        }
+        return error;
+    }
+    predict(number) {
+        const input = encodeNumber(number);
+        return this.feedForward(input);
+    }
+}
+
 function create(inputSize, hiddenSize, learningRate = 0.1) {
   return {
     inputSize,
