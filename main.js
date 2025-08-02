835a6f5d03cc9d96aaf3b7c70390b7a29f760446
andyblaze <andy@mediagraphic.co.uk>
2025-08-02 14:28:34 +0100

    minor tweaks and introduction of jquery and bootstrap

--- main.js
+++ main.js
@@ -3,20 +3,22 @@ import Trainer from "./trainer.js";
 import TestHarness from "./test-harness.js";
 import Visualiser from "./visualiser.js";
 
-const net = new Dave(3, 10);
-const trainer = new Trainer(net);
-const test = new TestHarness();
-const visualiser = new Visualiser("nnCanvas", 800, 800);
-trainer.loop(net);
-test.prediction(net);
-visualiser.setNetwork(net);
-const settings = {
-    baseX: 20,
-    xShift: 110,
-    headingHeight: 25,
-    boxSize: 70,
-    neuronRadius: 20,
-    headingGap: 40
-};
-visualiser.config(settings);
-window.visualiser = visualiser;
+$(document).ready(function() {
+    const net = new Dave(3, 10);
+    const trainer = new Trainer(net);
+    const test = new TestHarness();
+    const visualiser = new Visualiser("nnCanvas", 800, 800);
+    trainer.loop(net);
+    test.prediction(net);
+    visualiser.setNetwork(net);
+    const settings = {
+        baseX: 20,
+        xShift: 110,
+        headingHeight: 25,
+        boxSize: 70,
+        neuronRadius: 20,
+        headingGap: 40
+    };
+    visualiser.config(settings);
+    $("#visrun").on("click", () => visualiser.run());
+});
